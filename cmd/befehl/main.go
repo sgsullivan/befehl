cd0702974a3ecc7f6843591dfc9073be616dc38f
Scott Sullivan <scottgregorysullivan@gmail.com>
2016-11-30 08:06:22 -0500

    lets not allow default args

--- cmd/befehl/main.go
+++ cmd/befehl/main.go
@@ -7,17 +7,24 @@ import (
 	"os"
 )
 
-func main() {
-	pwd, err := os.Getwd()
-	if err != nil {
-		panic(fmt.Sprintf("%s", err))
-	}
+func showUsage() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+	flag.PrintDefaults()
+	os.Exit(1)
+}
 
-	targets := flag.String("targets", pwd+"/targets.txt", "The location to the txt file with ips of target machines run the payload on")
-	payload := flag.String("payload", pwd+"/payload.sh", "The location to the payload shell script")
+func main() {
+	targets := flag.String("targets", "REQUIRED", "The location to the txt file with ips of target machines run the payload on")
+	payload := flag.String("payload", "REQUIRED", "The location to the payload shell script")
 	routines := flag.Int("routines", 30, "Number of allowed concurrent Go routines")
 
 	flag.Parse()
+	if *targets == "REQUIRED" {
+		showUsage()
+	}
+	if *payload == "REQUIRED" {
+		showUsage()
+	}
 
 	fmt.Printf("\nusing targets: [%s] payload: [%s] routines: [%d]\n\n", *targets, *payload, *routines)
 	befehl.Fire(targets, payload, routines)
