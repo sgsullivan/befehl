bdd8032faf00603866bafa5c8fa8a27afa2c889e
Scott Sullivan <scottgregorysullivan@gmail.com>
2022-08-13 00:50:03 -0400

    rename

--- helpers/slice/unique.go
+++ helpers/slice/unique.go
@@ -2,14 +2,14 @@ package slice
 
 func Unique[T comparable](list []T) []T {
 	seen := make(map[T]bool)
-	uniqueList := []T{}
+	result := []T{}
 
 	for _, item := range list {
 		if _, exists := seen[item]; !exists {
 			seen[item] = true
-			uniqueList = append(uniqueList, item)
+			result = append(result, item)
 		}
 	}
 
-	return uniqueList
+	return result
 }
