1a4ddb9a84bc892cb9398df31361ff1547b22f5e
Scott Sullivan <scottgregorysullivan@gmail.com>
2022-08-13 01:01:36 -0400

    exercise the generic

--- helpers/slice/unique_test.go
+++ helpers/slice/unique_test.go
@@ -6,14 +6,23 @@ import (
 )
 
 func TestUnique(t *testing.T) {
-	a := []string{"foo", "bar", "bar", "baz"}
-	b := []string{"foo", "bar", "baz"}
+	as := []string{"foo", "bar", "bar", "baz"}
+	bs := []string{"foo", "bar", "baz"}
 
-	if !reflect.DeepEqual(Unique(a), b) {
+	if !reflect.DeepEqual(Unique(as), bs) {
 		t.Fatal("didn't remove duplicates")
 	}
-	if !reflect.DeepEqual(Unique(b), b) {
+	if !reflect.DeepEqual(Unique(bs), bs) {
 		t.Fatal("didn't preserve list if no duplicates")
 	}
 
+	ai := []int{1, 2, 3, 3, 4}
+	bi := []int{1, 2, 3, 4}
+
+	if !reflect.DeepEqual(Unique(ai), bi) {
+		t.Fatal("didn't remove duplicates")
+	}
+	if !reflect.DeepEqual(Unique(bi), bi) {
+		t.Fatal("didn't preserve list if no duplicates")
+	}
 }
